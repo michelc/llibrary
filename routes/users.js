c0d2aba8426aa85f7b88ffea56ef4a2a823190ca
Michel <michelc@gmail.com>
2018-09-27 13:52:43 +0200

    Fonstions arrows et guillemets.

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get("/", function(req, res, next) {
+router.get("/", (req, res, next) => {
   res.send("respond with a resource");
 });
 
 /* GET cool users listing. */
-router.get("/cool", function(req, res, next) {
+router.get("/cool", (req, res, next) => {
   res.send("You're so cool");
 });
 
